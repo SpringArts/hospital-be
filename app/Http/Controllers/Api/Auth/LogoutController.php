cdf63789f75df2ffb631ae2c72e53c40b663433c
MinHeinKoKo <kokominhein96@gmail.com>
2024-02-09 21:38:01 +0630

    finish auth

--- app/Http/Controllers/Api/Auth/LogoutController.php
+++ app/Http/Controllers/Api/Auth/LogoutController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Usecases\Auth\LogoutAction;
 use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
-    //
+    public function logout()
+    {
+        return (new LogoutAction)();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\ResetPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class , 'login']);
+Route::post('/password/email',  [ResetPasswordController::class, 'sendEmail']);
+Route::post('/password/reset', [ResetPasswordController::class, 'reset']);
